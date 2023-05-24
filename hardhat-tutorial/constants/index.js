94fbc67df74317f3a023b1ef91f3aa6e6cda3954
prnvsan <pranavhnairxd@gmail.com>
2023-05-24 15:29:03 +0530

    changes made

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x98f5c62F7b5Bc735a64942dBec9456954c7a368A";
+const WHITELIST_CONTRACT_ADDRESS = "0x11f9b526BDF7fa8f7a058DFDA21e5B9b1CB32cCf";
 // URL for the Crypto Devs NFT Metadata
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://whitelistdapp-phi.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
